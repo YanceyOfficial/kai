3a841633814025c20d82d83086b86d294821286a
YanceyOfficial <yanceyofficial@gmail.com>
2024-07-03 13:18:00 +0800

    feat: resolve confilct

--- src/components/AudioPlayer/index.tsx
+++ src/components/AudioPlayer/index.tsx
@@ -1,24 +1,33 @@
-import { FC, useEffect } from 'react'
-import { TouchableOpacity } from 'react-native'
+import { useSetAtom } from 'jotai'
+import LottieView from 'lottie-react-native'
+import { FC, useEffect, useRef } from 'react'
 import Sound from 'react-native-sound'
 import { YOUDAO_VOICE_URL } from '../../shared/constants'
-import AudioPlayingIcon from '../Icon/AudioPlayingIcon'
+import { isPlayingAtom } from '../../stores/global'
+import Button from '../Button'
 
 interface Props {
   word: string
 }
 
 const AudioPlayer: FC<Props> = ({ word }) => {
+  const animationRef = useRef<LottieView>(null)
+  const setIsPlaying = useSetAtom(isPlayingAtom)
+
   const handleAudio = async () => {
+    animationRef.current?.play()
+    setIsPlaying(true)
+
     Sound.setCategory('Playback')
     const whoosh = new Sound(`${YOUDAO_VOICE_URL}${word}`, undefined, () => {
-      // Play the sound with an onEnd callback
       whoosh.play((success) => {
         if (success) {
           console.log('successfully finished playing')
         } else {
           console.log('playback failed due to audio decoding errors')
         }
+        animationRef.current?.reset()
+        setIsPlaying(false)
       })
     })
   }
@@ -28,9 +37,16 @@ const AudioPlayer: FC<Props> = ({ word }) => {
   }, [word])
 
   return (
-    <TouchableOpacity onPress={handleAudio}>
-      <AudioPlayingIcon width={64} height={64} />
-    </TouchableOpacity>
+    <Button color="blue" wrapperClassNames="p-2" onPress={handleAudio}>
+      <LottieView
+        ref={animationRef}
+        source={require('../../../assets/lotties/lottie-sound.json')}
+        style={[{ width: 48, height: 48 }]}
+        loop
+        progress={1}
+        autoPlay={false}
+      />
+    </Button>
   )
 }
 

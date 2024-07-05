b5e19ae60e88d7cd62155917d956fc9d8ab87a52
YanceyOfficial <yanceyofficial@gmail.com>
2024-07-05 18:08:21 +0800

    feat: try react-native-device-info

--- src/screens/Configration/index.tsx
+++ src/screens/Configration/index.tsx
@@ -3,6 +3,7 @@ import Button from 'components/Button'
 import SafeAreaViewWrapper from 'components/SafeAreaViewWrapper'
 import useAuth from 'hooks/useAuth'
 import { FC } from 'react'
+import { View } from 'react-native'
 import { RootStackParamList } from 'types'
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Configuration'>
@@ -10,28 +11,30 @@ type Props = NativeStackScreenProps<RootStackParamList, 'Configuration'>
 const Configuration: FC<Props> = ({ navigation }) => {
   const { handleLogout } = useAuth()
   return (
-    <SafeAreaViewWrapper>
-      <Button color="blue" onPress={() => navigation.replace('CMS')}>
-        CMS
-      </Button>
-      <Button
-        color="blue"
-        wrapperClassNames="my-4"
-        onPress={() => navigation.replace('Login')}
-      >
-        LOGIN
-      </Button>
-      <Button color="blue" onPress={() => navigation.replace('System')}>
-        SYSTEM
-      </Button>
-      <Button
-        color="white"
-        variant="outlined"
-        wrapperClassNames="my-4"
-        onPress={handleLogout}
-      >
-        LOGOUT
-      </Button>
+    <SafeAreaViewWrapper wrapperClassNames="justify-start">
+      <View className='mt-8'>
+        <Button color="blue" onPress={() => navigation.replace('CMS')}>
+          CMS
+        </Button>
+        <Button
+          color="blue"
+          wrapperClassNames="my-4"
+          onPress={() => navigation.replace('Login')}
+        >
+          LOGIN
+        </Button>
+        <Button color="blue" onPress={() => navigation.replace('System')}>
+          SYSTEM
+        </Button>
+        <Button
+          color="white"
+          variant="outlined"
+          wrapperClassNames="my-4"
+          onPress={handleLogout}
+        >
+          LOGOUT
+        </Button>
+      </View>
     </SafeAreaViewWrapper>
   )
 }
--- src/screens/System/index.tsx
+++ src/screens/System/index.tsx
@@ -1,13 +1,166 @@
-import { FC } from 'react'
-import { Text } from 'react-native'
-import { getApplicationName } from 'react-native-device-info'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import SafeAreaViewWrapper from 'components/SafeAreaViewWrapper'
+import { FC, useEffect, useState } from 'react'
+import { ScrollView, Text, View } from 'react-native'
+import * as DeviceInfo from 'react-native-device-info'
+
+const Item: FC<{
+  name: string
+  value: string | number
+}> = ({ name, value }) => (
+  <Text className="mb-4">
+    <Text className="font-bold">{name}: </Text>
+    {value}
+  </Text>
+)
 
 const System: FC = () => {
+  const [deviceToken, setDeviceToken] = useState('')
+
+  const availableLocationProviders =
+    DeviceInfo.getAvailableLocationProvidersSync()
+
+  const powerStateSync = DeviceInfo.getPowerStateSync()
+
+  const getDeviceToken = async () => {
+    try {
+      const token = await DeviceInfo.getDeviceToken()
+      setDeviceToken(token)
+    } catch {}
+  }
+
+  useEffect(() => {
+    getDeviceToken()
+  }, [])
+
   return (
-    <SafeAreaView className="p-4">
-      <Text>Application Name: {getApplicationName()}</Text>
-    </SafeAreaView>
+    <SafeAreaViewWrapper wrapperClassNames="justify-start">
+      <ScrollView className="mt-4">
+        <Item name="Application Name" value={DeviceInfo.getApplicationName()} />
+
+        <View>
+          <Text className="font-bold">Available Location Providers:</Text>
+          <View className="ml-4 mt-4">
+            {Object.keys(availableLocationProviders).map((provider) => (
+              <Item
+                key={provider}
+                name={provider}
+                value={availableLocationProviders[provider].toString()}
+              />
+            ))}
+          </View>
+        </View>
+
+        <Item name="Build Id" value={DeviceInfo.getBuildIdSync()} />
+        <Item name="Battery Level" value={DeviceInfo.getBatteryLevelSync()} />
+        <Item name="Brand" value={DeviceInfo.getBrand()} />
+        <Item name="Build Number" value={DeviceInfo.getBuildNumber()} />
+        <Item name="Bundle Id" value={DeviceInfo.getBundleId()} />
+        <Item name="Carrier" value={DeviceInfo.getCarrierSync()} />
+        <Item name="Device Id" value={DeviceInfo.getDeviceId()} />
+        <Item name="Device Type" value={DeviceInfo.getDeviceType()} />
+        <Item name="Device Name" value={DeviceInfo.getDeviceNameSync()} />
+        <Item name="Device Token" value={deviceToken} />
+        <Item
+          name="First Install Name"
+          value={DeviceInfo.getFirstInstallTimeSync()}
+        />
+        <Item name="Font Scale" value={DeviceInfo.getFontScaleSync()} />
+        <Item
+          name="Free Disk Storage"
+          value={DeviceInfo.getFreeDiskStorageSync()}
+        />
+        <Item
+          name="Free Disk Storage Old"
+          value={DeviceInfo.getFreeDiskStorageOldSync()}
+        />
+        <Item name="Ip Address" value={DeviceInfo.getIpAddressSync()} />
+        <Item name="Carrier" value={DeviceInfo.getApplicationName()} />
+
+        <Item
+          name="Installer Package Name"
+          value={DeviceInfo.getInstallerPackageNameSync()}
+        />
+        <Item name="Mac Address" value={DeviceInfo.getMacAddressSync()} />
+        <Item name="Manufacturer" value={DeviceInfo.getManufacturerSync()} />
+        <Item name="Model" value={DeviceInfo.getModel()} />
+
+        <View>
+          <Text className="font-bold">Power State:</Text>
+          <View className="ml-4 mt-4">
+            {Object.keys(powerStateSync).map((item) => (
+              <Item
+                key={item}
+                name={item}
+                value={powerStateSync[item].toString()}
+              />
+            ))}
+          </View>
+        </View>
+
+        <Item name="Readable Version" value={DeviceInfo.getReadableVersion()} />
+        <Item name="System Name" value={DeviceInfo.getSystemName()} />
+        <Item name="System Version" value={DeviceInfo.getSystemVersion()} />
+        <Item
+          name="Total Disk Capacity"
+          value={DeviceInfo.getTotalDiskCapacitySync()}
+        />
+        <Item
+          name="Total Disk Capacity Old"
+          value={DeviceInfo.getTotalDiskCapacityOldSync()}
+        />
+        <Item name="Total Memory" value={DeviceInfo.getTotalMemorySync()} />
+        <Item name="Unique Id" value={DeviceInfo.getUniqueIdSync()} />
+        <Item name="Used Memory" value={DeviceInfo.getUsedMemorySync()} />
+        <Item name="Version" value={DeviceInfo.getVersion()} />
+        <Item name="Brightness" value={DeviceInfo.getBrightnessSync()} />
+        <Item name="Has Notch" value={DeviceInfo.hasNotch().toString()} />
+        <Item
+          name="Has Dynamic Island"
+          value={DeviceInfo.hasDynamicIsland().toString()}
+        />
+        <Item
+          name="Is Battery Charging"
+          value={DeviceInfo.isBatteryChargingSync().toString()}
+        />
+        <Item
+          name="Is Emulator"
+          value={DeviceInfo.isEmulatorSync().toString()}
+        />
+        <Item
+          name="Is Landscape"
+          value={DeviceInfo.isLandscapeSync().toString()}
+        />
+        <Item
+          name="Is Location Enabled"
+          value={DeviceInfo.isLocationEnabledSync().toString()}
+        />
+        <Item
+          name="Is Headphones Connected"
+          value={DeviceInfo.isHeadphonesConnectedSync().toString()}
+        />
+        <Item
+          name="Is Wired Headphones Connected"
+          value={DeviceInfo.isWiredHeadphonesConnectedSync().toString()}
+        />
+        <Item
+          name="Is Bluetooth Headphones Connected"
+          value={DeviceInfo.isBluetoothHeadphonesConnectedSync().toString()}
+        />
+        <Item
+          name="Is Pin Or Fingerprint Set"
+          value={DeviceInfo.isPinOrFingerprintSetSync().toString()}
+        />
+        <Item name="Is Tablet" value={DeviceInfo.isTablet().toString()} />
+        <Item
+          name="Is Display Zoomed"
+          value={DeviceInfo.isDisplayZoomed().toString()}
+        />
+        <Item
+          name="Supported Abis"
+          value={DeviceInfo.supportedAbisSync().join('; ')}
+        />
+      </ScrollView>
+    </SafeAreaViewWrapper>
   )
 }
 

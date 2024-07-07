67f92127a4d01f3d43e332f48a7d0ae1698f34cb
YanceyOfficial <yanceyofficial@gmail.com>
2024-07-07 21:42:53 +0800

    feat: support safe area

--- src/screens/CMS/index.tsx
+++ src/screens/CMS/index.tsx
@@ -1,14 +1,16 @@
 import { FC, useState } from 'react'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { WebView } from 'react-native-webview'
 import Loading from 'src/components/Loading'
 
 const CmsWebview: FC = () => {
   const [loading, setLoading] = useState(false)
+  const { top } = useSafeAreaInsets()
   return (
     <>
       {loading && <Loading fullScreen />}
       <WebView
-        source={{ uri: 'https://kai.yancey.app/' }}
+        source={{ uri: `https://kai.yancey.app/?safeAreaTop=${top}` }}
         className={loading ? 'hidden' : ''}
         onLoadStart={() => {
           setLoading(true)
